5a3fd32954a6352573b9ad86222f84bf85cad9ec
busseauc77 <busseauc@gmail.com>
2023-06-24 20:08:23 -0400

    remise tp3

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+      <section class="py-5">
+          <div class="container px-4 px-lg-5 my-5">
+              <div class="row gx-4 gx-lg-5 align-items-center">
+                  <div class="col-md-6">
+                      <h1 class="display-5 fw-bolder">About Us</h1>
+                      <div class="fs-5 mb-5">
+                          <h2>Maisonneuve College - June 2023</h2>
+                          <h3>Techniques Web Avancées</h3>
+                          <span>Vue.js School Project</span>
+                      </div>
+                      <p class="lead">Made with love and hard work and the help of a wonderful teacher with great patience and skill.</p>
+                      <small>Camille Busseau - 2195352</small>
+                      <hr class="my-4">
+                      <div class="d-flex">
+                        <RouterLink class="nav-link active" aria-current="page" to="/"><a class="btn btn-secondary">Home</a></RouterLink>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+    </div>
+</template>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+    <!-- Section-->
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <div v-for="(product, i) in inventory" :key="i" class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" :src="require(`@/assets/img/450/${product.photo}`)" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">{{ product.name }}</h5>
+                                <!-- Product price-->
+                                ${{ product.price.toFixed(2) }}
+                            </div>
+                            <span>{{ product.type }}</span>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center">
+                                <input type="number" class="form-control qt" v-model.number="product.quantity">
+                                <div class="d-flex flex-column row-gap-2">
+                                    <a class="btn btn-outline-dark mt-auto" @click="add(product.name, i)">Add to the
+                                        cart</a>
+
+                                    <RouterLink :to="{ name: 'product', params: { id: product.name } }">
+                                        <a class="btn btn-outline-dark mt-auto link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Details</a>
+                                    </RouterLink>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  props: ['inventory', 'add']
+}
+</script>
